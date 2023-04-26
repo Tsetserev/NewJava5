e78d261e22f95d3312d39f884a0482eac9db548c
Aleksandr <tsetserev123@gmail.com>
2023-04-26 12:52:58 +0300

    change code

--- src/test/java/ru/netology/services/calculateSrvTest.java
+++ src/test/java/ru/netology/services/calculateSrvTest.java
@@ -5,24 +5,25 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+class calculateSrvTest {
     @Test
     public void testCountRestMonths() {
-        Main service = new Main();
+        calculateSrv service = new calculateSrv();
 
-        int actual = Main.calculate(10_000,3_000, 20_000);
+        int actual = calculateSrv.calculate(10_000,3_000, 20_000);
         int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
     @Test
     public void testCountRestMonths2() {
-        Main service = new Main();
+        calculateSrv service = new calculateSrv();
 
-        int actual = Main.calculate(100_000, 60_000, 150_000);
+        int actual = calculateSrv.calculate(100_000, 60_000, 150_000);
         int expected = 2;
 
         Assertions.assertEquals(expected, actual);
     }
+
 
 }
